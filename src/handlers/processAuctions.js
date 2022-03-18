8c98fc4f459fd5d30f2cf7735d37e925f47b9e9e
ShubhamAbnaveInfogenLabs <shubham.a@infogen-labs.com>
2022-03-18 17:22:19 +0530

    getEndedAuctions added

--- src/handlers/processAuctions.js
+++ src/handlers/processAuctions.js
@@ -1,4 +1,7 @@
+import { getEndedAuctions } from "../lib/getEndedAuctions";
 const processAuctions = async (event,context) => {
+    const auctionsToClose = await getEndedAuctions();
+    console.log(auctionsToClose);
 console.log("Processing Auction");
 };
 export const handler = processAuctions;
